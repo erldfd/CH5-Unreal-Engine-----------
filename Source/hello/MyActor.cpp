651fdc466a0580f2f29f8add455a7417a53e513f
erldfd <hseongrae@gmail.com>
2025-01-06 11:10:00 +0900

    Initial commit

---
+++ Source/hello/MyActor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	int MinValue = 1;
+	int MaxValue = 45;
+
+	int TotalNumerCount = 6;
+
+	TArray<int> SelectedNumbers;
+
+	for (int i = 0; i < TotalNumerCount; ++i)
+	{
+		int SelectedNumber = 0;
+
+		do
+		{
+			SelectedNumber = FMath::RandRange(MinValue, MaxValue);
+		} 
+		while (SelectedNumbers.Contains(SelectedNumber));
+
+		SelectedNumbers.Add(SelectedNumber);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Lotto Numbers: %d, %d, %d, %d, %d, %d"), SelectedNumbers[0], SelectedNumbers[1], SelectedNumbers[2], SelectedNumbers[3], SelectedNumbers[4], SelectedNumbers[5]);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
